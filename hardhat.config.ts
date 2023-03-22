6b48d67d31a98af3e67de3d1331effe5cc77c470
Kevin Oh <kevoh1516@gmail.com>
2023-03-22 13:14:29 -0400

    remove abi exporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,45 +1,38 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "@nomiclabs/hardhat-etherscan";
-import "solidity-coverage";
-import "hardhat-abi-exporter";
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+import '@nomiclabs/hardhat-etherscan'
+import 'solidity-coverage'
+import 'hardhat-abi-exporter'
 
-import * as dotenv from "dotenv";
-dotenv.config();
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 const config: HardhatUserConfig = {
-  solidity: {
-    version: "0.8.17",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-        details: {
-          yul: false,
+    solidity: {
+        version: '0.8.17',
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+                details: {
+                    yul: false,
+                },
+            },
         },
-      },
     },
-  },
-  abiExporter: {
-    path: "./abi",
-    format: "json",
-    runOnCompile: true,
-    clear: true,
-    flat: true,
-  },
-  networks: {
-    goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [process.env.METAMASK_PRIVATE_KEY!],
+    networks: {
+        goerli: {
+            url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+            accounts: [process.env.METAMASK_PRIVATE_KEY!],
+        },
+        sepolia: {
+            url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+            accounts: [process.env.METAMASK_PRIVATE_KEY!],
+        },
     },
-    sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [process.env.METAMASK_PRIVATE_KEY!],
+    etherscan: {
+        apiKey: `${process.env.ETHERSCAN_API_KEY}`,
     },
-  },
-  etherscan: {
-    apiKey: `${process.env.ETHERSCAN_API_KEY}`,
-  },
-};
+}
 
-export default config;
+export default config
